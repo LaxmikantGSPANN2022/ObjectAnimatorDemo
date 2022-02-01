2406df1bb6e3c4fcaa3660ec568c60bd5162d546
Laxmikant Mahamuni <Laxmikant.Mahamuni@nike.com>
2022-02-01 22:12:58 +0530

    Initial commit

---
+++ app/src/world/java/com/example/objectanimatordemo/ObjectAnimatorWorldApplication.java
@@ -0,0 +1,10 @@
+package com.example.objectanimatordemo;
+
+import android.app.Application;
+
+public class ObjectAnimatorApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

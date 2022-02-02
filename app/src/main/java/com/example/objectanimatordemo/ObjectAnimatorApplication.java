0603c223cde175d86d6f38f37fa28afae9dcbb81
Laxmikant Mahamuni <Laxmikant.Mahamuni@nike.com>
2022-02-02 11:51:49 +0530

    Manifest change

--- app/src/main/java/com/example/objectanimatordemo/ObjectAnimatorApplication.java
+++ app/src/main/java/com/example/objectanimatordemo/ObjectAnimatorApplication.java
@@ -1,2 +1,12 @@
-package com.example.objectanimatordemo;public class ObjectAnimatorApplication {
+package com.example.objectanimatordemo;
+
+import android.app.Application;
+import android.widget.Toast;
+
+public class ObjectAnimatorApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(getApplicationContext(), "Hello in Object Animator!", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/world/java/com/example/objectanimatordemo/ObjectAnimatorWorldApplication.java
+++ app/src/world/java/com/example/objectanimatordemo/ObjectAnimatorWorldApplication.java
@@ -1,10 +1,12 @@
 package com.example.objectanimatordemo;
 
 import android.app.Application;
+import android.widget.Toast;
 
-public class ObjectAnimatorApplication extends Application {
+public class ObjectAnimatorWorldApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Toast.makeText(getApplicationContext(), "Hello in Object Animator For World", Toast.LENGTH_SHORT).show();
     }
 }
